4b02a88972a6fe111ffbf9a1efc957ef9be5cf06
Michael Lewis <michael.lewis@springbok.ai>
2024-01-21 23:58:40 +0000

    tidy more, sort back button, lint, add score based responses

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,90 @@
-import React, {useState} from 'react';
-import './App.css';
-import { motion } from 'framer-motion';
-import StarMatch from './components/StarMatch';
-
+import React, { useState } from 'react'
+import './App.css'
+import { motion } from 'framer-motion'
+import StarMatch from './components/StarMatch'
 
 const App = () => {
-  const [gameId, setGameId] = useState(0);
-  const [selectedNumber, setSelectedNumber] = useState(5);
-  const [selectedDifficulty, setSelectedDifficulty] = useState('easy');
+    const [gameId, setGameId] = useState(0)
+    const [selectedNumber, setSelectedNumber] = useState(5)
+    const [selectedDifficulty, setSelectedDifficulty] = useState('easy')
 
-  const handleChangeNumber = (event) => {
-    setSelectedNumber(event.target.value);
-  };
+    const handleChangeNumber = (event) => {
+        setSelectedNumber(event.target.value)
+    }
 
-  const handleChangeDifficulty = (event) => {
-    setSelectedDifficulty(event.target.value);
-  };
+    const handleChangeDifficulty = (event) => {
+        setSelectedDifficulty(event.target.value)
+    }
 
-  const startCounter = () => {
-   const i = 1; 
-   setGameId(i);
-  }
+    const startCounter = () => {
+        const i = 1
+        setGameId(i)
+    }
 
-  const startGame = () => {
-  if (gameId === 0) {
-   return;
-   } else {
-   return (
-    <>
-    <button onClick={() => setGameId(0)}>Back</button>
-    <StarMatch key={gameId} selectedNumber={selectedNumber} selectedDifficulty={selectedDifficulty} startNewGame={() => setGameId(gameId + 1)}/>
-    </>
-  );}
-  }
+    const startGame = () => {
+        if (gameId === 0) {
+            return
+        } else {
+            return (
+                <>
+                    
+                    <StarMatch
+                        key={gameId}
+                        selectedNumber={selectedNumber}
+                        selectedDifficulty={selectedDifficulty}
+                        startNewGame={() => setGameId(gameId + 1)}
+                    />
+                </>
+            )
+        }
+    }
 
-  return (
-  <div className="home-page">
-              <motion.h1
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1 }}
-          >
-            Star Math Game
-          </motion.h1>
-      {gameId === 0 &&
-      (<> <h2>How to play:</h2>
-      <div>
-
-      <p>Welcome to the Star Game. This is a game to test your mental arithmetic.</p>
-      <p>First choose a difficulty, and a number of stars below. (more stars will be a longer and more challenging game!)</p>
-      <p>stars will appear in the left panel and numbers will appear in the right panel</p>
-      <p>You must click <b>1 or more</b> numbers that add up to the number of stars displayed</p>
-      <p>Numbers will turn blue when selected, green when used successfully and red if they won't work for the current stars</p>
-      <p>Your aim is to get all of the numbers to turn green before the timer runs out!</p>
-      </div>
-            <select value={selectedNumber} onChange={handleChangeNumber}>
-        {[...Array(16).keys()].map(i => 
-          <option key={i} value={i + 5}>{i + 5}</option>
-        )}
-      </select>
-      <select value={selectedDifficulty} onChange={handleChangeDifficulty}>
-        {['beginner', 'moderate', 'hard'].map(i => 
-          <option key={i} value={i}>{i}</option>
-        )}
-      </select><button className="start-game-button" onClick={startCounter}>Start Game</button> </>)}
-      <div>{startGame()}</div>
-  </div>
-  );
+    return (
+        <div className='home-page'>
+            <motion.h1 initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1 }}>
+                Star Math Game
+            </motion.h1>
+            {gameId !== 0 && <button className='back-button' onClick={() => setGameId(0)}>Back to Rules & Settings</button>}
+            {gameId === 0 && (
+                <>
+                    {' '}
+                    <h2>How to play:</h2>
+                    <div>
+                        <p>Welcome to the Star Game. This is a game to test your mental arithmetic.</p>
+                        <p>To begin, please choose a difficulty and a number of stars below. (more stars will be a longer and more challenging game!)</p>
+                        <p>stars will appear in the left panel and numbers will appear in the right panel</p>
+                        <p>
+                            You must click <b>1 or more</b> numbers that add up to the number of stars displayed
+                        </p>
+                        <p>Numbers will turn blue when selected, green when used successfully and red if they won't work for the current stars</p>
+                        <p>Your aim is to get all of the numbers to turn green before the timer runs out!</p>
+                    </div>
+                    <div>
+                        <label htmlFor='number'>Number of Stars:</label>
+                        <select value={selectedNumber} onChange={handleChangeNumber}>
+                            {[...Array(16).keys()].map((i) => (
+                                <option key={i} value={i + 5}>
+                                    {i + 5}
+                                </option>
+                            ))}
+                        </select>
+                        <label htmlFor='difficulty'>Difficulty:</label>
+                        <select value={selectedDifficulty} onChange={handleChangeDifficulty}>
+                            {['beginner', 'moderate', 'hard'].map((i) => (
+                                <option key={i} value={i}>
+                                    {i}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <button className='start-game-button' onClick={startCounter}>
+                        Start Game
+                    </button>{' '}
+                </>
+            )}
+            <div>{startGame()}</div>
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/components/PlayAgain.jsx
+++ src/components/PlayAgain.jsx
@@ -1,8 +1,20 @@
 import { motion } from 'framer-motion'
 import { colors } from './Utils'
 
-const PlayAgain = (props) => (
-    <div className='game-done'>
+const scoreLevels = [
+    { score: 600, message: "Congratulations, you're a certified Star Game Wizard!" },
+    { score: 500, message: "Amazing!... Nearly Perfect. Keep trying!" },
+    { score: 400, message: "Wow, impressive score" },
+    { score: 300, message: "You're getting good at this" },
+    { score: 200, message: "Nice work, keep trying to get a better score" },
+    { score: 100, message: "Good game, go faster to improve your score" },
+    { score: 0, message: "Well done, try a harder difficulty to get a better score"}
+  ];
+
+const PlayAgain = (props) => {
+    const scoreMessage = scoreLevels.find(level => props.score >= level.score).message;
+
+    return (<div className='game-done'>
         <div
             className='message'
             style={{ color: props.gameStatus === 'lost' ? 'red' : 'green' }}
@@ -26,10 +38,11 @@ const PlayAgain = (props) => (
             )}
         </div>
         <div>Your Score was: {props.score}</div>
+        <h3>{scoreMessage}</h3>
         <button className='restart-button' onClick={props.startNewGame}>
             Play Again
         </button>
-    </div>
-)
+    </div>)
+}
 
 export default PlayAgain
